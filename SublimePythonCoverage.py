af68ae3ca1b1feecfafa05a1bc92cfd89ffe99ee
Shane Hathaway <shane@wingcash.com>
2017-12-20 16:59:46 -0700

    Detect the test class and function the user is pointing at so the runner can run that test as a pretest.

--- SublimePythonCoverage.py
+++ SublimePythonCoverage.py
@@ -152,6 +152,9 @@ except ImportError:
 
 class TestExecCommand(ExecCommand):
     """An extension of the default build system. Shows coverage at the end.
+
+    Also tries to tell the test runner exactly which function and class the
+    user is pointing at so it can run a specific pretest.
     """
 
     runner = None
@@ -162,6 +165,31 @@ class TestExecCommand(ExecCommand):
     def run(self, **kw):
         if 'cmd' not in kw:
             fname = self.window.active_view().file_name()
+            env = {'TESTFILE': fname}
+
+            if fname.endswith('.py'):
+                try:
+                    view = self.window.active_view()
+                    content = view.substr(sublime.Region(0, view.size()))
+                    point = view.sel()[0].begin()
+                    row, _col = view.rowcol(point)
+                    decls = list_decls(content, fname)
+                    bottom_decl = find_decl_for_row(decls, row)
+                    attrs = []
+                    for decl in reversed(list(bottom_decl.lineage())):
+                        if isinstance(decl, ClassDecl):
+                            attrs.append(decl.name)
+                        elif isinstance(decl, FuncDecl):
+                            attrs.append(decl.name)
+                            break
+                        else:
+                            break
+                    if attrs:
+                        env['TESTATTR'] = '.'.join(attrs)
+                except Exception as e:
+                    print(
+                        "SublimePythonCoverage parser failed: "
+                        "%s %s" % (type(e), e))
 
             # look for a virtualenv with nosetests, py.test etc
             runner = find_cmd(fname, self.runner)
@@ -176,7 +204,7 @@ class TestExecCommand(ExecCommand):
                 kw['working_dir'] = os.path.dirname(testpath)
 
             kw['cmd'] = self.cmd(runner, testpath)
-            kw['env'] = {'TESTFILE': fname}
+            kw['env'] = env
 
         super(TestExecCommand, self).run(**kw)
 
@@ -204,3 +232,134 @@ class PytestExecCommand(TestExecCommand):
 
     def cmd(self, runner, testpath):
         return [runner]
+
+
+# The code below is a little library for discovering the method
+# and class name the user is pointing at.
+
+
+import ast
+import re
+import weakref
+
+
+empty_line_re = re.compile(r'\s*$')
+
+
+class Decl(object):
+    """A function or class declaration statement."""
+    def __init__(self, name, first_row, last_row=None, children=None):
+        self.name = name
+        self.first_row = first_row
+        # Note: last_row includes the blank rows after the declaration.
+        self.last_row = last_row
+        self.children = children or []
+        self.parent_ref = None  # A weakref.ref
+
+    def lineage(self):
+        decl = self
+        while decl is not None:
+            yield decl
+            parent = decl.parent_ref()
+            if parent is None or isinstance(parent, ModuleDecl):
+                break
+            decl = parent
+
+    def __repr__(self):
+        return ('{0}({1!r}, {2!r}, {3!r}, {4!r})'
+                .format(self.__class__.__name__,
+                        self.name,
+                        self.first_row,
+                        self.last_row,
+                        self.children))
+
+
+class ModuleDecl(Decl):
+    pass
+
+
+class ClassDecl(Decl):
+    pass
+
+
+class FuncDecl(Decl):
+    pass
+
+
+class Visitor(ast.NodeVisitor):
+    """Create a Decl tree from a Python abstract syntax tree."""
+
+    def __init__(self, lines):
+        self.parent = self.top = ModuleDecl('', 0)
+        self.lines = lines
+        self.last_lineno = 1
+        self.closing_decls = []
+
+    def visitdecl(self, node, cls):
+        decl = cls(node.name, node.lineno - 1)
+        parent = self.parent
+        decl.parent_ref = weakref.ref(parent)
+        parent.children.append(decl)
+        self.last_lineno = node.lineno
+        self.parent = decl
+        self.generic_visit(node)
+        self.parent = parent
+        decl.last_row = self.last_lineno - 1
+        self.closing_decls.append(decl)
+
+    def visit_FunctionDef(self, node):
+        self.close_decls(node.lineno)
+        self.visitdecl(node, FuncDecl)
+
+    def visit_ClassDef(self, node):
+        self.close_decls(node.lineno)
+        self.visitdecl(node, ClassDecl)
+
+    def generic_visit(self, node):
+        if hasattr(node, 'lineno'):
+            self.close_decls(node.lineno)
+            self.last_lineno = max(self.last_lineno, node.lineno)
+        super(Visitor, self).generic_visit(node)
+
+    def close_decls(self, new_lineno):
+        decls = self.closing_decls
+        if decls:
+            # Change the range of the closing declarations to include
+            # multi-line expressions, but not blank lines.
+            # To compute last_row, subtract 1 from new_lineno because
+            # the previous declaration ends on the line before;
+            # subtract 1 again because rows are zero-based while lines are
+            # one-based.
+            last_row = new_lineno - 2
+            while last_row > 0 and last_row < len(self.lines):
+                line = self.lines[last_row]
+                if not line or empty_line_re.match(line):
+                    # Ignore an empty line.
+                    last_row -= 1
+                else:
+                    break
+            for decl in decls:
+                decl.last_row = max(last_row, decl.last_row)
+            del self.closing_decls[:]
+
+
+def list_decls(content, filename):
+    """List the nested declarations in a module."""
+    node = ast.parse(content, filename)
+    lines = content.splitlines()
+    visitor = Visitor(lines)
+    visitor.visit(node)
+    if visitor.closing_decls:
+        visitor.close_decls(len(lines) + 1)
+    return visitor.top.children
+
+
+def find_decl_for_row(decls, row):
+    for decl in decls:
+        if row >= decl.first_row and row <= decl.last_row:
+            child = find_decl_for_row(decl.children, row)
+            if child is not None:
+                return child
+            else:
+                return decl
+    return None
